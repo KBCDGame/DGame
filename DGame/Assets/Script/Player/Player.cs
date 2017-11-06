e3867ff6621a1d8db57699fe679ce49bf4391c24
NoborioYoshiki <a>
2017-11-06 10:47:42 +0900

    ゲーム制作用の土台を作ったのでPush

---
+++ DGame/Assets/Script/Player/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    //移動速度。
+    private float moveSpeed = 1.0f;
+    //回転速度。
+    private float rotateSpeed = 3.0f;
+
+    //座標計算用。
+    float moveX = 0.0f, moveZ = 0.0f;
+    // Use this for initialization
+    void Start () {
+        //座標初期化。
+        moveX = transform.localPosition.x;
+        moveZ = transform.localPosition.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //入力がローカルプレイヤーのみによって処理されるようにする。
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        //座標計算。
+        moveX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        moveZ = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        //回転の処理。
+        Vector3 direction = new Vector3(moveX, 0, moveZ);
+        if (direction.magnitude > 0.01f)
+        {
+            float step = rotateSpeed * Time.deltaTime;
+            Quaternion myQ = Quaternion.LookRotation(direction);
+            this.transform.rotation = Quaternion.Lerp(transform.rotation, myQ, step);
+        }
+
+        //座標と回転を反映。
+        transform.Translate(moveX, 0.0f, moveZ, Space.World);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        //1Pカラー。
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
---
+++ DGame/Assets/Script/Title/Button/StartButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class StartButton : MonoBehaviour {
+    void Start()
+    {
+        // Buttonクリック時、OnClickメソッドを呼び出す
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        // 「GameScene」シーンに遷移する
+        SceneManager.LoadScene("DGame");
+    }
+}
