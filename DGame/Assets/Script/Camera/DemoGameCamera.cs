a66ed94dbd3f4a658bbf165bf39dae03fac1187a
NoborioYoshiki <a>
2017-11-22 10:34:38 +0900

    カメラの前後の移動追加

--- DGame/Assets/Script/Camera/DemoGameCamera.cs
+++ DGame/Assets/Script/Camera/DemoGameCamera.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class DemoGameCamera : MonoBehaviour {
 
+    //プレイヤー。
     private GameObject Player = null;
+    //プレイヤーの位置。
     private Vector3 PlayerPos = Vector3.zero;
+    //入力値保持用。
+    private float InputX, InputY, Scroll = 0.0f;
+    //カメラ。
+    private Camera GemaCamera = null;
+    ////ズームに使う値保持用。
+    //private float Scroll, View = 0.0f;
 
     [SerializeField]
     //回転速度。
@@ -14,12 +22,19 @@ public class DemoGameCamera : MonoBehaviour {
     public float MaxRotateY = 0.0f;
     //下に対する回転の下限。
     public float MinRotateY = 0.0f;
+    ////ズームの最大と最小。
+    //public float ZoomMax,ZoomMin = 0.0f;
+    ////ズームの時のスピード。
+    //public float ZoomSpeed = 1.0f;
+    //
+
 
     // Use this for initialization
     void Start()
     {
         Player = GameObject.FindWithTag("Player");
         PlayerPos = Player.transform.position;
+        GemaCamera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -29,27 +44,70 @@ public class DemoGameCamera : MonoBehaviour {
         transform.position += Player.transform.position - PlayerPos;
         PlayerPos = Player.transform.position;
 
+        //Input関係の処理。
+        InputRotate();
+
+        //ズーム。
+        //CameraZoom();
+
+       
+        //PlayerPosの位置のY軸を中心に、回転（公転）する
+        transform.RotateAround(PlayerPos, Vector3.up, InputX);
+
+        //前後のカメラ移動。
+        if (Input.GetKey(KeyCode.Joystick1Button4))
+        {
+            CameraMoveBeforeAndAfter();
+        }
+        else
+        {
+            //カメラの垂直移動（角度制限なし）
+            transform.RotateAround(PlayerPos, transform.right, InputY);
+        }
+            
+    }
+
+    //Inputの回転をまとまたもの。
+   private void InputRotate()
+    {
+        float DeltaSpeed = Time.deltaTime * RotateSpeed;
         // マウスの右クリックを押している間
         if (Input.GetMouseButton(1))
         {
             //マウスの移動量
-            float mouseInputX = Input.GetAxis("Mouse X") * Time.deltaTime * RotateSpeed;
-            float mouseInputY = Input.GetAxis("Mouse Y") * Time.deltaTime * RotateSpeed;
-            //PlayerPosの位置のY軸を中心に、回転（公転）する
-            transform.RotateAround(PlayerPos, Vector3.up, mouseInputX);
-            //カメラの垂直移動（角度制限なし）
-            transform.RotateAround(PlayerPos, transform.right, mouseInputY );
+            InputX = Input.GetAxis("Mouse X") * DeltaSpeed;
+            InputY = Input.GetAxis("Mouse Y") * DeltaSpeed;
         }
         //マウスの右が押されていない時は右スティックの倒しを優先する。
         else
-        { 
+        {
             //右スティックの移動量
-            float InputX = Input.GetAxis("Horizontal2") * Time.deltaTime * RotateSpeed;
-            float InputY = Input.GetAxis("Vertical2") * Time.deltaTime * RotateSpeed;
-            //PlayerPosの位置のY軸を中心に、回転（公転）する
-            transform.RotateAround(PlayerPos, Vector3.up, InputX);
-            //カメラの垂直移動（角度制限なし）
-            transform.RotateAround(PlayerPos, transform.right, InputY);
+            InputX = Input.GetAxis("Horizontal2") * DeltaSpeed;
+            InputY = Input.GetAxis("Vertical2") * DeltaSpeed;
+        }
+
+        //ホイールの入力。
+        Scroll = Input.GetAxis("Mouse ScrollWheel");
+    }
+
+    ////カメラのズーム処理。
+    //private void CameraZoom()
+    //{
+    //    float view = GemaCamera.fieldOfView - Scroll * ZoomSpeed;
+    //    //Clampを使ってマイナスや極端に大きな値にならなよう調整。
+    //    GemaCamera.fieldOfView = Mathf.Clamp(value: view, min:ZoomMin, max: ZoomMax);
+    //}
+
+    //ホイールとパッドを使ったカメラの前後移動。
+    private void CameraMoveBeforeAndAfter()
+    {
+        //カメラの移動。
+        //transform.position.Set(transform.position.x, transform.position.y, transform.position.z + -InputY * Time.deltaTime * 10.0f);
+        //float distance = Vector3.Distance(transform.position, PlayerPos);
+        if (Mathf.Abs(Vector3.Distance((transform.forward * -InputY * Time.deltaTime * 10.0f),PlayerPos)) < 80.0f && Mathf.Abs(Vector3.Distance((transform.forward * -InputY * Time.deltaTime * 10.0f), PlayerPos)) > 3.0f)
+        {
+            transform.position += transform.forward * -InputY * Time.deltaTime * 10.0f;
         }
     }
+
 }
--- DGame/Assets/Script/Player/DemoPlayer.cs
+++ DGame/Assets/Script/Player/DemoPlayer.cs
@@ -22,7 +22,6 @@ public class DemoPlayer : MonoBehaviour {
     //移動方向。
     private Vector3 MoveDirection = Vector3.zero;
 
-
     // Use this for initialization
     void Start()
     {
@@ -49,6 +48,74 @@ public class DemoPlayer : MonoBehaviour {
                 Jump();
             }
         }
+
+        //if (Input.GetKey(KeyCode.Joystick1Button0))
+        //{
+        //    Debug.Log("Button A Push");
+        //}
+        //if (Input.GetKey(KeyCode.Joystick1Button1))
+        //{
+        //    Debug.Log("Button B Push");
+        //}
+        //if (Input.GetKey(KeyCode.Joystick1Button2))
+        //{
+        //    Debug.Log("Button X Push");
+        //}
+        //if (Input.GetKey(KeyCode.Joystick1Button3))
+        //{
+        //    Debug.Log("Button Y Push");
+        //}
+        //if (Input.GetKey(KeyCode.Joystick1Button4))
+        //{
+        //    Debug.Log("Button LB Push");
+        //}
+        //if (Input.GetKey(KeyCode.Joystick1Button5))
+        //{
+        //    Debug.Log("Button RB Push");
+        //}
+        //if (Input.GetKey(KeyCode.Joystick1Button6))
+        //{
+        //    Debug.Log("Button Back Push");
+        //}
+        //if (Input.GetKey(KeyCode.Joystick1Button7))
+        //{
+        //    Debug.Log("Button START Push");
+        //}
+        //if (Input.GetKey(KeyCode.Joystick1Button8))
+        //{
+        //    Debug.Log("L Stick Push Push");
+        //}
+        //if (Input.GetKey(KeyCode.Joystick1Button9))
+        //{
+        //    Debug.Log("R Stick Push");
+        //}
+        //float TrigerInput = Input.GetAxis("Triger");
+        //if (TrigerInput < 0.0f)
+        //{
+        //    Debug.Log("L Triger");
+        //}
+        //else if (TrigerInput > 0.0f)
+        //{
+        //    Debug.Log("R Triger");
+        //}
+        //float HorizontalKeyInput = Input.GetAxis("HorizontalKey");
+        //if (HorizontalKeyInput < 0.0f)
+        //{
+        //    Debug.Log("Left Key");
+        //}
+        //else if (HorizontalKeyInput > 0.0f)
+        //{
+        //    Debug.Log("Right Key");
+        //}
+        //float VerticalKeyInput = Input.GetAxis("VerticalKey");
+        //if (VerticalKeyInput < 0.0f)
+        //{
+        //    Debug.Log("Up Key");
+        //}
+        //else if (VerticalKeyInput > 0.0f)
+        //{
+        //    Debug.Log("Down Key");
+        //}
     }
 
     //移動を安定させるためにFixedUpdateにした。
